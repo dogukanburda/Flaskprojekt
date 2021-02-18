da58310318323ed007cd00cd64f6c69b6cdbd1e0
dogukanburda <dogukankefeli@gmail.com>
2021-02-18 20:54:31 +0300

    self refreshing page and data flow

--- web.py
+++ web.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
+import numpy as np
+from apscheduler.schedulers.background import BackgroundScheduler
+app = Flask(__name__,static_url_path='')
 
-app = Flask(__name__)
+label=np.arange(1024)
+value=np.zeros(1024)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+
 
 @app.route("/")
 def home():
     return render_template("giris.html")
 @app.route("/graph")
 def graph():
-    return render_template("fullgraph.html")
+    
+    return render_template("fullgraph.html",title='ASD',  label=label.tolist(), value=value.tolist())
 
 
 @app.route("/admin")
@@ -28,7 +47,14 @@ def dedektor(numara):
 
 
 
-
+def load_data():
+    print("çalıştı")
+    for k in range(10000):
+        i = round(np.random.normal(5,0.1)*100)
+        value[i] = value[i]+1
 
 if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=load_data, trigger="interval", seconds=10)
+    scheduler.start()
     app.run(debug=True)
